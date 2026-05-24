4ca5dab47b0483d82bb5813a7842307579178f9a
Sparsh Chandra <sparshhchandra1706@gmail.com>
2026-05-24 19:01:38 +0530

    Updated project

--- client/src/apiCalls/index.js
+++ client/src/apiCalls/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const url = "https://quick-chat-app-rql3.onrender.com";
+export const url = "https://chitchat-2h7z.onrender.com";
 
 export const axiosInstance = axios.create({
     headers: {
