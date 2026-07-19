f09df571b37e3de7d269c1c9fb436f95d06a3cf6
Sparsh Chandra <sparshhchandra1706@gmail.com>
2026-07-19 15:36:22 +0530

    Fix Render port configuration

--- server/server.js
+++ server/server.js
@@ -7,9 +7,7 @@ const server = require('./app');
 const { suggestReply } = require('./controllers/aiController'); 
 const authMiddleware = require('./middlewares/authMiddleware');  // 
 
-
-const port = process.env.PORT_NUMBER || 3000;
-
+const port = process.env.PORT || process.env.PORT_NUMBER || 3000;
 server.listen(port, () => {
     console.log('Listening to requests on PORT: ' + port);
 });
