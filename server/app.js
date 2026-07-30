70e0bc58f13d6b9f81740214314a3ca2904157e5
Sparsh Chandra <sparshhchandra1706@gmail.com>
2026-07-31 00:08:13 +0530

    Add Vercel production URL to CORS

--- server/app.js
+++ server/app.js
@@ -12,10 +12,7 @@ const authMiddleware = require('./middlewares/authMiddleware');
 // FIXED CORS: Added all matching dev and production addresses
 const allowedOrigins = [
   'http://localhost:3000',
-  'http://localhost:5000',
-  'https://chit-chat-ch2it5pta-sparsh-s-projects4.vercel.app',
-  'https://chit-chat-app-puce.vercel.app',
-  'https://chit-chat-app-main-sparshs-projects4.vercel.app'
+  'https://chit-chat-jocc-qfwso7je0-sparsh-s-projects4.vercel.app/login'
 ];
 app.use(cors({ 
   origin: allowedOrigins, 
