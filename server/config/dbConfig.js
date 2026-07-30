0edebe3f5a4156253d5ee86448a6799bd48f55ab
Sparsh Chandra <sparshhchandra1706@gmail.com>
2026-07-30 19:45:28 +0530

    Fix server port and database connection format

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+
+
+
 //Connection logic
 mongoose.connect(process.env.CONN_STRING);
 
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ console.log("CHECKING KEY:", process.env.GEMINI_API_KEY ? "FOUND" : "NOT FOUND")
 const dbconfig = require('./config/dbConfig');
 const server = require('./app');
 const { suggestReply } = require('./controllers/aiController'); 
-const authMiddleware = require('./middlewares/authMiddleware');  // 
+const authMiddleware = require('./middlewares/authMiddleware'); 
 
 const port = process.env.PORT || process.env.PORT_NUMBER || 3000;
 server.listen(port, () => {
